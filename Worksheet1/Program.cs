73633bcdda9836960387a5f9105b66495fc1e8a3
S00221640 <S00221640@LAB-D2039-18>
2023-01-25 10:11:45 +0000

    Add project files.

---
+++ Worksheet1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Worksheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void PrintIntegersFor(int[] args)
+        {
+            int num1 = 40;
+            Console.WriteLine(num1);
+            for (num1 < 60)
+                num1 = num1 + 1;
+        }
+    }
+}
