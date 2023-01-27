1233d57f0a80359fb6048e7044e5cfe768587d70
s00221640 <123546995+s00221640@users.noreply.github.com>
2023-01-27 10:03:26 +0000

    Add files via upload
    Updated worksheet 1 question 1.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Worksheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintIntegersFor();
+        }
+        static void PrintIntegersFor()
+        {
+
+            for (int i = 40; i <= 60; i++)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine(i);
+                
+            }
+          
+        }
+    }
+}
